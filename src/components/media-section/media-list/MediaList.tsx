14065bf811ba4be07ce453ec42cff3c08ca2e81b
Nikita Panasevich <nikita.panasevitch@yandex.by>
2024-12-22 23:46:07 +0300

    added viewport

--- src/components/media-section/media-list/MediaList.tsx
+++ src/components/media-section/media-list/MediaList.tsx
@@ -9,6 +9,7 @@ const MediaList = ({media} : MediaListProps) => {
         <motion.div className={styles.container}
                     initial={{ opacity: 0, scale: 0.8 }}
                     whileInView={{ opacity: 1, scale: 1 }}
+                    viewport={{ once: true }}
                     transition={{ duration: 0.8 }}>
             <div className={styles.content}>
                 {media.map((media) => (
--- src/components/media-section/media-search/MediaSearch.tsx
+++ src/components/media-section/media-search/MediaSearch.tsx
@@ -7,6 +7,7 @@ const MediaSearch = () => {
         <motion.div className={styles.container}
                     initial={{ opacity: 0, scale: 0.8 }}
                     whileInView={{ opacity: 1, scale: 1 }}
+                    viewport={{ once: true }}
                     transition={{ duration: 0.8 }}>
             <input type='text' placeholder='Поиск...' />
         </motion.div>

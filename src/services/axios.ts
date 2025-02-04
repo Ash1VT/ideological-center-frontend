11bb740c73d8c35625ab5f8d29565feafe73c470
Nikita Panasevich <nikita.panasevitch@yandex.by>
2025-02-05 01:35:36 +0300

    mixed content

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,8 +2,12 @@ import axios from "axios";
 import { backendUrl } from "src/constants/backend";
 
 export const backend = axios.create({
-    withCredentials: true,
-    baseURL: backendUrl
+    withCredentials: false,
+    baseURL: backendUrl,
+    headers: {
+       'Access-Control-Allow-Origin': '*',
+       'Content-Type': 'application/json',
+    },
 })
 
 axios.interceptors.response.use(function (response) {

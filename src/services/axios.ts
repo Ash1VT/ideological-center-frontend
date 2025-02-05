815b426ca10b84ffb05ed44c091cc41fbe1a325a
Nikita Panasevich <nikita.panasevitch@yandex.by>
2025-02-05 19:40:26 +0300

    removed stuff

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,12 +2,8 @@ import axios from "axios";
 import { backendUrl } from "src/constants/backend";
 
 export const backend = axios.create({
-    withCredentials: false,
+    withCredentials: true,
     baseURL: backendUrl,
-    headers: {
-       'Access-Control-Allow-Origin': '*',
-       'Content-Type': 'application/json',
-    },
 })
 
 axios.interceptors.response.use(function (response) {
